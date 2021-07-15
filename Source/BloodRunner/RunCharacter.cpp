b648e8f6c5a70780797d08a7f4d89ad50886df40
MurilinhoPs <murilinhorps@gmail.com>
2021-07-14 22:51:43 -0300

    Create PlayerHealth getter and setter to introduce c++

--- Source/BloodRunner/RunCharacter.cpp
+++ Source/BloodRunner/RunCharacter.cpp
@@ -1,6 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "RunCharacter.h"
 
 // Sets default values
@@ -8,26 +5,33 @@ ARunCharacter::ARunCharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	PlayerHealth = 0.3f;
 }
 
 // Called when the game starts or when spawned
 void ARunCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
 
 // Called every frame
-void ARunCharacter::Tick(float DeltaTime)
+void ARunCharacter::Tick(float const DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
 
 // Called to bind functionality to input
 void ARunCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
 }
 
+void ARunCharacter::SetPlayerHealth(float const Health)
+{
+	PlayerHealth += Health;	
+}
+
+float ARunCharacter::GetPlayerHealth() const
+{
+	return PlayerHealth;
+}
